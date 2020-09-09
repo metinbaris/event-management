0866e335d71861cf5c08e366358d865cc62f917e
metinbaris <hizmetparki@gmail.com>
2020-09-09 16:06:36 +0300

    db set

--- app/UserEvent.php
+++ app/UserEvent.php
@@ -8,4 +8,6 @@ class UserEvent extends Model
 {
     protected $table = 'user_events';
     protected $fillable = ['user_id', 'event_id', 'token', 'email_verified_at', 'qrcode_verified_at'];
+    protected $primaryKey = ['user_id', 'event_id'];
+    public $incrementing = false;
 }
