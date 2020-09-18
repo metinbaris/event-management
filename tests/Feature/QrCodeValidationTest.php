59eb2ee5e0089fc3ce0b9795d89f4f4781e5cdba
metinbaris <hizmetparki@gmail.com>
2020-09-18 14:55:41 +0300

    qrcodevalidation test

---
+++ tests/Feature/QrCodeValidationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\UserEvent;
+use Tests\TestCase;
+
+class QrCodeValidationTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testQrValidation()
+    {
+        $response = $this->post('/validate-qrcode', [
+            'email' => 'qr_validation_test_123456000000@gmail.com',
+            'companyEvent' => 'istanbul-boat-party',
+            'token' => 'sample_token_123456'
+        ]);
+
+        $response->assertDontSeeText('fail');
+        $response->assertStatus(200);
+        $this->removeDatabaseInsertions();
+    }
+
+    private function removeDatabaseInsertions()
+    {
+        $user = User::where('email', 'qr_validation_test_123456000000@gmail.com')->first();
+        $userEvent = UserEvent::where('user_id', $user->id)->first();
+        $userEvent->update(['qrcode_verified_at' => null]);
+    }
+}
